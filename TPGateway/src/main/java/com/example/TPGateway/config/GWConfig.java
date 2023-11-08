1e1b8f72b885dff5f7263b31d8ae839f038ec283
SantiiJuarez <juarezshhmm@gmail.com>
2023-11-08 17:34:44 -0300

    gateway

--- TPGateway/src/main/java/com/example/TPGateway/config/GWConfig.java
+++ TPGateway/src/main/java/com/example/TPGateway/config/GWConfig.java
@@ -4,11 +4,17 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
 @Configuration
-//@EnableWebFluxSecurity
+@EnableWebFluxSecurity
 public class GWConfig {
 
     @Bean
@@ -21,43 +27,44 @@ public class GWConfig {
 
     }
 
-//    @Bean
-//    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
-//        http.authorizeExchange(exchanges -> exchanges
-//
-//                        // Esta ruta puede ser accedida por cualquiera, sin autorización
-//                        .pathMatchers("/api/personas/**")
-//                        .hasRole("KEMPES_ADMIN")
-//
-//                        .pathMatchers("/api/entradas/**")
-//                        .hasRole("KEMPES_ORGANIZADOR")
-//
-//                        // Cualquier otra petición...
-//                        .anyExchange()
-//                        .authenticated()
-//
-//                ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-//                .csrf(csrf -> csrf.disable());
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
-//        var jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
-//        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-//
-//        // Se especifica el nombre del claim a analizar
-//        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
-//        // Se agrega este prefijo en la conversión por una convención de Spring
-//        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
-//
-//        // Se asocia el conversor de Authorities al Bean que convierte el token JWT a un objeto Authorization
-//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
-//                new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
-//        // También se puede cambiar el claim que corresponde al nombre que luego se utilizará en el objeto
-//        // Authorization
-//        // jwtAuthenticationConverter.setPrincipalClaimName("user_name");
-//
-//        return jwtAuthenticationConverter;
-//    }
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        http.authorizeExchange(exchanges -> exchanges
+
+                        // Esta ruta puede ser accedida por cualquiera, sin autorización
+                        .pathMatchers("/get/**")
+                        .hasRole("USUARIO")
+
+                        .pathMatchers("/api/estaciones/**")
+                        .hasRole("ADMIN")
+
+                        // Cualquier otra petición...
+                        .anyExchange()
+                        .authenticated()
+
+                ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        // Se especifica el nombre del claim a analizar
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        // Se agrega este prefijo en la conversión por una convención de Spring
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        // Se asocia el conversor de Authorities al Bean que convierte el token JWT a un objeto Authorization
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+                new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
+        // También se puede cambiar el claim que corresponde al nombre que luego se utilizará en el objeto
+        // Authorization
+        // jwtAuthenticationConverter.setPrincipalClaimName("user_name");
+
+        return jwtAuthenticationConverter;
+    }
 }
+
